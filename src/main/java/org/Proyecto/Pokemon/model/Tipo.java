b88206aa80967d013203285696732cf5e8c83024
GonzaloMarco <117201012+gmarcogracia@users.noreply.github.com>
2023-04-17 12:19:23 +0200

    Añadido el calculo de dtodos los tipos menos hada, las ebilidades, stab y daño

--- src/main/java/org/Proyecto/Pokemon/model/Tipo.java
+++ src/main/java/org/Proyecto/Pokemon/model/Tipo.java
@@ -2,7 +2,7 @@ package org.Proyecto.Pokemon.model;
 
 public enum Tipo {
     AGUA,PLANTA,FUEGO,ROCA,TIERRA,ACERO,ELECTRICO, NORMAL,LUCHA,FANTASMA,
-    PSIQUICO,VOLADOR, VENENO;
+    PSIQUICO,VOLADOR, VENENO,DRAGON,BICHO,HIELO,SINIESTRO;
 
     /*
 Posible ejemplo de como puede funcionar  la comprobacion de efectividad de los movimientos y tipos
