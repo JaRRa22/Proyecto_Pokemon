f36d77e82c3fcc96ecafbe17f472f14489c2bc40
JaRRa <117831787+JaRRa22@users.noreply.github.com>
2023-04-17 12:38:20 +0200

    Update clase Tipo
    Cambio de contenido de la rama gonzalo a la clase primerMerge

--- src/main/java/org/Proyecto/Pokemon/model/Tipo.java
+++ src/main/java/org/Proyecto/Pokemon/model/Tipo.java
@@ -2,17 +2,15 @@ package org.Proyecto.Pokemon.model;
 
 public enum Tipo {
     AGUA,PLANTA,FUEGO,ROCA,TIERRA,ACERO,ELECTRICO, NORMAL,LUCHA,FANTASMA,
-    PSIQUICO,VOLADOR, VENENO;
+    PSIQUICO,VOLADOR, VENENO,DRAGON,BICHO,HIELO,SINIESTRO;
 
     /*
 Posible ejemplo de como puede funcionar  la comprobacion de efectividad de los movimientos y tipos
 IDEALMENTE CON UN SWITCH
     public float comprobar(Tipo tipejo){
-
         if (this.equals(AGUA) && tipejo.equals(FUEGO)){
             return 2.0f;
         }
-
         return 0;
     }   */
 }
