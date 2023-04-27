a3bf85ea9c7c917d234b158dfb84a649dd076b03
BreynerA <123334798+BreynerA@users.noreply.github.com>
2023-04-27 21:57:39 +0200

    Controlador de la vista de inicio

--- src/main/java/org/Proyecto_Pokemon/controller/InicioController.java
+++ src/main/java/org/Proyecto_Pokemon/controller/InicioController.java
@@ -2,21 +2,28 @@ package org.Proyecto_Pokemon.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import org.Proyecto_Pokemon.model.Entrenador;
 import org.Proyecto_Pokemon.model.Pokemon;
 import org.Proyecto_Pokemon.model.Tienda;
 import org.Proyecto_Pokemon.model.TipoPokeball;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
 
 public class InicioController {
-    private static Entrenador usuario;
+    protected static Entrenador usu;
     @FXML
     private Button btnSignIn;
 
@@ -32,17 +39,20 @@ public class InicioController {
     @FXML
     private ImageView imagenPokemon;
 
+    private Parent root;
+    private Stage stage;
+
+    private Scene scene;
 
 
     public void initialize(){
         File f = new File("Proyecto_Pokemon\\src\\main\\resources\\imagenes\\pokemon-2.gif");
         Image image = new Image(f.toURI().toString());
         imagenPokemon.setImage(image);
-
     }
 
 
-    public void registro(ActionEvent event){
+    public void registro(ActionEvent event) throws IOException, InterruptedException {
         if(introNombre.getText().equals("Nombre") || introNombre.getText().length() == 0 || (introNombre.getText().length() > 0 && introNombre.getText().length()<= 3) || introNombre.getText().length() > 10){
             chulo.setText("✗");
             info.setText("ERROR: Nombre no válido");
@@ -52,8 +62,17 @@ public class InicioController {
 
             chulo.setText("✓");
             info.setText("¡Bienvenido " +"("+introNombre.getText()+")"+ "!");
-            usuario = new Entrenador(introNombre.getText());
+            usu = new Entrenador(introNombre.getText());
+
+            root = FXMLLoader.load(getClass().getResource("/fxml/Menu.fxml"));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        }
+
         }
 
     }
-}
+
